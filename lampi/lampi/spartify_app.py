646ea08014772124ac37c13ed4561fd96dfc736a
Ubuntu <ubuntu@ip-172-31-82-35.ec2.internal>
2022-04-28 21:30:25 +0000

    buncha stuff

--- lampi/lampi/spartify_app.py
+++ lampi/lampi/spartify_app.py
@@ -1,7 +1,142 @@
 import kivy
-
 from kivy.app import App
-# from kivy.uix.label import Label
+from kivy.properties import StringProperty, NumericProperty, BooleanProperty, AliasProperty
+import platform
+from kivy.uix.label import Label
+from math import fabs
+import json
+import os
+from paho.mqtt.client import Client
+import pigpio
+from lamp_common import *
+import lampi.lampi_util
+
+MQTT_CLIENT_ID = "lamp_spartify"
 
 class SpartifyApp(App):
-    pass
+    album_image = StringProperty("images/albumCovers/Before You Go.JPG.png")
+    _updated = False
+    _updatingUI = False
+    _hue = NumericProperty()
+    _saturation = NumericProperty()
+    _brightness = NumericProperty()
+    lamp_is_on = BooleanProperty()
+
+    def _get_hue(self):
+        return self._hue
+
+    def _set_hue(self, value):
+        self._hue = value
+
+    def _get_saturation(self):
+        return self._saturation
+    
+    def _get_title(self, value):
+        return self.album_image
+
+    def _set_saturation(self, value):
+        self._saturation = value
+
+    def _get_brightness(self):
+        return self._brightness
+
+    def _set_brightness(self, value):
+        self._brightness = value
+    
+    def _set_title(self, value):
+        self.album_image = value + ".PNG"
+
+    album_image = StringProperty(_get_title, _set_title, bind = ['album_image'])
+    hue = AliasProperty(_get_hue, _set_hue, bind=['_hue'])
+    saturation = AliasProperty(_get_saturation, _set_saturation,
+                               bind=['_saturation'])
+    brightness = AliasProperty(_get_brightness, _set_brightness,
+                               bind=['_brightness'])
+    gpio17_pressed = BooleanProperty(False)
+    device_associated = BooleanProperty(True)
+    def on_start(self):
+        self._publish_clock = None
+        self.mqtt_broker_bridged = False
+        self._associated = True
+        self.association_code = None
+        self.mqtt = Client(client_id=MQTT_CLIENT_ID)
+        self.mqtt.enable_logger()
+        self.mqtt.will_set(client_state_topic(MQTT_CLIENT_ID), "0",
+                           qos=2, retain=True)
+        self.mqtt.on_connect = self.on_connect
+        self.mqtt.connect(MQTT_BROKER_HOST, port=MQTT_BROKER_PORT,
+                          keepalive=MQTT_BROKER_KEEP_ALIVE_SECS)
+        self.mqtt.loop_start()
+        self.set_up_GPIO_and_device_status_popup()
+        self.associated_status_popup = self._build_associated_status_popup()
+        self.associated_status_popup.bind(on_open=self.update_popup_associated)
+        Clock.schedule_interval(self._poll_associated, 0.1)
+
+        # replace with the binding to the skip pause and play buttons
+    def on_lamp_is_on(self, instance, value):
+        if self._updatingUI:
+            return
+        self._track_ui_event('Toggle Power', {'isOn': value})
+        if self._publish_clock is None:
+            self._publish_clock = Clock.schedule_once(
+                lambda dt: self._update_leds(), 0.01)
+
+    def on_connect(self, client, userdata, flags, rc):
+        self.mqtt.publish(client_state_topic(MQTT_CLIENT_ID), b"1",
+                          qos=2, retain=True)
+        self.mqtt.message_callback_add(TOPIC_LAMP_CHANGE_NOTIFICATION,
+                                       self.receive_new_lamp_state)
+        self.mqtt.message_callback_add(broker_bridge_connection_topic(),
+                                       self.receive_bridge_connection_status)
+        self.mqtt.message_callback_add(TOPIC_LAMP_ASSOCIATED,
+                                       self.receive_associated)
+        self.mqtt.subscribe(broker_bridge_connection_topic(), qos=1)
+        self.mqtt.subscribe(TOPIC_LAMP_CHANGE_NOTIFICATION, qos=1)
+        self.mqtt.subscribe(TOPIC_LAMP_ASSOCIATED, qos=2)
+
+    def _poll_associated(self, dt):
+        # this polling loop allows us to synchronize changes from the
+        #  MQTT callbacks (which happen in a different thread) to the
+        #  Kivy UI
+        self.device_associated = self._associated
+
+
+    def receive_bridge_connection_status(self, client, userdata, message):
+        # monitor if the MQTT bridge to our cloud broker is up
+        if message.payload == b"1":
+            self.mqtt_broker_bridged = True
+        else:
+            self.mqtt_broker_bridged = False
+
+    def receive_new_lamp_state(self, client, userdata, message):
+        new_state = json.loads(message.payload.decode('utf-8'))
+        Clock.schedule_once(lambda dt: self._update_ui(new_state), 0.01)
+
+    def _update_ui(self, new_state):
+        self._updatingUI = True
+        try:
+            if 'title' in new_state:
+                self.title = new_state['title']
+            if 'color' in new_state:
+                self.hue = new_state['color']['h']
+                self.saturation = new_state['color']['s']
+            if 'brightness' in new_state:
+                self.brightness = new_state['brightness']
+            #always set to on for now
+            self.lamp_is_on = new_state['on']
+        finally:
+            self._updatingUI = False
+
+        self._updated = True
+
+    def _update_leds(self):
+        msg = {'color': {'h': self._hue, 's': self._saturation},
+               'brightness': self._brightness,
+               'on': self.lamp_is_on,
+               'client': MQTT_CLIENT_ID}
+        self.mqtt.publish(TOPIC_SET_LAMP_CONFIG,
+                          json.dumps(msg).encode('utf-8'),
+                          qos=1)
+        self._publish_clock = None
+
+    
